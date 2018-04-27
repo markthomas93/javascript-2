42f842084c27298b920426df31da5ca76887ec63
Suwijak Chaipipat <ppnk2537@gmail.com>
2018-04-27 11:30:15 +0700

    Revert "Add node / es6 / browser / jest env"

--- index.js
+++ index.js
@@ -1,15 +1,8 @@
 module.exports = {
   extends: 'airbnb',
-  env: {
-    node: true,
-    es6: true,
-    browser: true,
-    jest: true,
-  },
   rules: {
     // No semi-colon
     semi: ['error', 'never'],
-
     // Trailing comma
     'comma-dangle': [
       'error',
@@ -21,5 +14,11 @@ module.exports = {
         functions: 'ignore',
       },
     ],
-  },
+    'react/jsx-filename-extension': [
+        1,
+        {
+          extensions: ['.js', '.jsx']
+        }
+    ],
+  }
 }
